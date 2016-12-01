a42ef178a14bfc2f188014ebf710ce0f8a04e5b8
Ma Junbao <mjbwork12@gmail.com>
2016-12-01 17:03:53 +0800

    gh-pages

---
+++ gh-pages.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+git checkout gh-pages
+git checkout master -- site
+rm  *.html
+rm -rf css images js components
+mv -f site/* ./
+rm -rf size
+git add .
+git commit -am "gh-pages"
+git push origin gh-pages
+
+git checkout master
